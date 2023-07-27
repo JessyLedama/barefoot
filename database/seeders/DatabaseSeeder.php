795f1a74e71e3ab5791ddd9463c6d3bdcb3a657e
Jessy Ledama <jaylisteledama94@gmail.com>
2023-07-27 14:44:05 +0300

    [CREATE]  created database seeder for Jayliste

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,11 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-<<<<<<< HEAD
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-=======
->>>>>>> 2107d1dac4dc6ae133eb91018a87e3945d5fca50
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,17 +16,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-<<<<<<< HEAD
         User::factory()->create([
             'name' => 'Jayliste',
             'email' => 'jayliste@gmail.com',
             'password' => Hash::make('password'),
         ]);
-=======
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
->>>>>>> 2107d1dac4dc6ae133eb91018a87e3945d5fca50
     }
 }
